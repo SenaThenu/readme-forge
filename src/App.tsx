271f5177733c732f3a0d7b61cdca3634da7909e0
Senash Thenuja <senathenu@gmail.com>
2024-10-12 02:12:26 +1300

    ✨ feat: raw wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Button } from "@mui/material";
-
 export default function App() {
     return (
-        <>
-            <Button variant="contained">Hello world</Button>
-        </>
+        <div id="app">
+            <div className="nav-bar"></div>
+            <div className="forge-area">
+                <div className="left-pane">
+                    <div className="selected-components"></div>
+                    <div className="available-components"></div>
+                </div>
+                <div className="component-form"></div>
+                <div className="markdown-preview"></div>
+            </div>
+        </div>
     );
 }
