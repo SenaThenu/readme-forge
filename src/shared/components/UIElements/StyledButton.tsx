3e40a4b61ea9c62587b2215b250883a588c6e1a7
Senash Thenuja <senathenu@gmail.com>
2024-10-13 00:55:17 +1300

    💄 style: more customized button

--- src/shared/components/UIElements/StyledButton.tsx
+++ src/shared/components/UIElements/StyledButton.tsx
@@ -1,8 +1,14 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
-const CustomButton = styled(Button)<ButtonProps>(() => ({
+const CustomButton = styled(Button)<ButtonProps>(({ theme }) => ({
     borderRadius: "25px",
+    boxShadow: "none !important",
+    textTransform: "none",
+    color:
+        theme.palette.mode === "dark"
+            ? theme.palette.text.primary // Dark theme color
+            : theme.palette.text.secondary, // Light theme color
 }));
 
 export default function StyledButton(props: ButtonProps) {
