c1e26b0e1f94fb7b610aadb4af9894c3b0e436e8
Senash Thenuja <senathenu@gmail.com>
2024-10-20 22:17:28 +1300

    💄 style: icon only styles for the button

--- src/shared/components/UIElements/StyledButton.tsx
+++ src/shared/components/UIElements/StyledButton.tsx
@@ -1,16 +1,27 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
-const CustomButton = styled(Button)<ButtonProps>(({ theme }) => ({
-    borderRadius: "25px",
-    boxShadow: "none !important",
-    textTransform: "none",
-    color:
-        theme.palette.mode === "dark"
-            ? theme.palette.text.primary // Dark theme color
-            : theme.palette.text.secondary, // Light theme color
-}));
+function getCustomButton(iconOnly: boolean) {
+    return styled(Button)<ButtonProps>(({ theme }) => ({
+        padding: iconOnly ? 0 : undefined,
+        height: "45px",
+        minWidth: iconOnly ? "45px" : undefined,
+        borderRadius: "25px",
+        boxShadow: "none !important",
+        textTransform: "none",
+        fontSize: "1rem",
+        color:
+            theme.palette.mode === "dark"
+                ? theme.palette.text.primary // Dark theme color
+                : theme.palette.text.secondary, // Light theme color
+        "& .MuiButton-startIcon": {
+            margin: iconOnly ? 0 : undefined,
+            marginRight: iconOnly ? 0 : "5px",
+        },
+    }));
+}
 
 export default function StyledButton(props: ButtonProps) {
+    const CustomButton = getCustomButton(props.children == null);
     return <CustomButton {...props} />;
 }
