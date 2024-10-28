e120a0ba0b5c28a82a34b0f9036eea018cb85263
Senash Thenuja <senathenu@gmail.com>
2024-10-28 22:32:19 +1300

    ✨ feat: markdown block data

---
+++ src/types/BlockCategoryType.ts
@@ -0,0 +1,9 @@
+import BlockDataType from "./BlockDataType";
+
+interface BlockCategoryType {
+    name: string;
+    displayName: string;
+    blocks: BlockDataType[]; 
+}
+
+export default BlockCategoryType
---
+++ src/types/BlockDataType.ts
@@ -0,0 +1,7 @@
+interface BlockDataType {
+    name: string;
+    description: string;
+    markdown: string;
+}
+
+export default BlockDataType
