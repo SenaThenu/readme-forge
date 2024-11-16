f1435073854a7b18a4de0abf07d977832ef4aec4
Senash Thenuja <senathenu@gmail.com>
2024-11-16 23:52:17 +1300

    ♻️ refactor: styled text field separated for ease of use

--- src/forge/components/SearchField.tsx
+++ src/forge/components/SearchField.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, ChangeEvent } from "react";
 
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
+// material ui components
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { useTheme } from "@mui/material/styles";
+
+// components
+import StyledTextField from "../../shared/components/UIElements/StyledTextField";
 
 interface SearchFieldProps {
     onSearchQueryChange: (newSearchQuery: string) => void;
@@ -11,7 +12,6 @@ interface SearchFieldProps {
 
 export default function SearchField(props: SearchFieldProps) {
     const [searchQuery, setSearchQuery] = useState("");
-    const theme = useTheme(); // Get current theme
 
     useEffect(() => {
         props.onSearchQueryChange(searchQuery);
@@ -22,41 +22,12 @@ export default function SearchField(props: SearchFieldProps) {
     };
 
     return (
-        <div
-            className="search-field-container"
-            style={{
-                borderRadius: "15px",
-                backgroundColor: theme.palette.secondary.main,
-                padding: "5px 10px",
-                width: "100%",
-            }}>
-            <TextField
-                id="standard-basic"
-                placeholder="Search..."
-                variant="standard"
-                value={searchQuery}
-                onChange={changeHandler}
-                fullWidth
-                sx={{
-                    "& .MuiInputBase-input": {
-                        fontSize: "1rem",
-                        fontWeight: "400",
-                    },
-                }}
-                slotProps={{
-                    input: {
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <SearchRoundedIcon
-                                    sx={{
-                                        color: theme.palette.text.primary,
-                                    }}
-                                />
-                            </InputAdornment>
-                        ),
-                    },
-                }}
-            />
-        </div>
+        <StyledTextField
+            id="search-field"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={changeHandler}
+            startIcon={<SearchRoundedIcon />}
+        />
     );
 }
