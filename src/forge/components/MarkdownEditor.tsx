581f9c4823ce5ef2c9899fbbffe7dce17f0d336e
Senash Thenuja <senathenu@gmail.com>
2024-11-18 00:44:01 +1300

    🐛 fix: separate markdown edit histories for each block

--- src/forge/components/MarkdownEditor.tsx
+++ src/forge/components/MarkdownEditor.tsx
@@ -1,7 +1,9 @@
-import CodeMirror from "@uiw/react-codemirror";
+import { useRef, useEffect, useState } from "react";
+import CodeMirror, { ReactCodeMirrorRef } from "@uiw/react-codemirror";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
 import { githubLight, githubDark } from "@uiw/codemirror-theme-github";
+import { history } from "@codemirror/commands";
 
 // material ui components
 import { useTheme } from "@mui/material/styles";
@@ -11,23 +13,42 @@ import "./MarkdownEditor.scss";
 
 interface MarkdownEditor {
     markdownInput: string;
+    resetEditHistory: boolean;
     handleChange: (rawMarkdownInput: string) => void;
+    handleFinishReset: () => void;
 }
 
-const extensions = [
+const baseExtensions = [
     markdown({ base: markdownLanguage, codeLanguages: languages }),
+    history(),
 ];
 
 export default function MarkdownEditor(props: MarkdownEditor) {
+    const editorRef = useRef<ReactCodeMirrorRef>(null);
     const theme = useTheme();
+    const [key, setKey] = useState(0); // add a key state to force remount
+
+    useEffect(() => {
+        if (props.resetEditHistory) {
+            setKey((prev) => prev + 1); // increment key to force remount
+        }
+
+        console.log("reset success!");
+        props.handleFinishReset();
+    }, [props.resetEditHistory]);
 
     return (
         <CodeMirror
+            key={key} // add key prop to force remount when it changes
+            ref={editorRef}
             value={props.markdownInput}
             onChange={(value) => props.handleChange(value)}
             className={`editor ${theme.palette.mode}`}
             theme={theme.palette.mode === "dark" ? githubDark : githubLight}
-            extensions={extensions}
+            extensions={[
+                ...baseExtensions,
+                theme.palette.mode === "dark" ? githubDark : githubLight,
+            ]}
         />
     );
 }
