2cdf7f3497062530e086b8137fc82a79a034ed43
Senash Thenuja <senathenu@gmail.com>
2024-12-01 01:51:03 +1300

    ✨ feat: basic route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from "./home/pages/Home";
+import Templates from "./templates/pages/Templates";
 import Forge from "./forge/pages/Forge";
+import NotFound from "./shared/pages/NotFound";
 
 // styles
 import "./styles/global.scss";
@@ -6,7 +12,21 @@ import "./styles/global.scss";
 export default function App() {
     return (
         <div id="app">
-            <Forge templateName="default" />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/templates" element={<Templates />} />
+                    <Route
+                        path="/forge"
+                        element={<Forge templateName="default" />}
+                    />
+                    <Route
+                        path="/forge/:name"
+                        element={<Forge templateName="default" />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
