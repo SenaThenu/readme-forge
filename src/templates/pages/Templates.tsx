647dee8cda7cf229282c0f736ce24c90fc7682c8
Senash Thenuja <senathenu@gmail.com>
2024-12-06 12:59:39 +1300

    ✨ feat: templates page raw wireframe

--- src/templates/pages/Templates.tsx
+++ src/templates/pages/Templates.tsx
@@ -1,16 +1,36 @@
+// components
 import NavLayout from "../../shared/components/Navigation/NavLayout";
+import TemplateGridItem from "../components/TemplateGridItem";
+
+// styles
+import "./Templates.scss";
 
 export default function Templates() {
     return (
         <div className="templates-root">
-            <NavLayout
-                navBarMode="default"
-                navLinks={[
-                    { text: "Home", link: "/" },
-                    { text: "Templates", link: "/templates" },
-                    { text: "Forge", link: "/forge" },
-                ]}
-            />
+            <div className="nav-container">
+                <NavLayout
+                    navBarMode="default"
+                    navLinks={[
+                        { text: "Home", link: "/" },
+                        { text: "Templates", link: "/templates" },
+                        { text: "Forge", link: "/forge" },
+                    ]}
+                />
+            </div>
+            <div className="templates-content">
+                <div className="templates-title">Templates</div>
+                <div className="templates-grid">
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                    <TemplateGridItem />
+                </div>
+            </div>
         </div>
     );
 }
