132cb7367ec398778af26ed34620d6f01722e636
Senash Thenuja <senathenu@gmail.com>
2024-12-08 01:54:31 +1300

    ✨ feat: new not-found-page bare wireframe

--- src/shared/pages/NotFound.tsx
+++ src/shared/pages/NotFound.tsx
@@ -1,3 +1,31 @@
+import { useEffect } from "react";
+
+// components
+import NavBar from "../components/Navigation/NavBar";
+
+// styles
+import "./NotFound.scss";
+
+// assets
+import notFoundImg from "../../assets/images/not-found/404.webp";
+
 export default function NotFound() {
-    return <div>404 Not Found! (Readme Forge)</div>;
+    useEffect(() => {
+        document.title = "404 Not Found - Readme Forge";
+    }, []);
+    return (
+        <div className="not-found-root">
+            <NavBar />
+            <div className="not-found-content">
+                <div className="not-found-img">
+                    <img
+                        src={notFoundImg}
+                        alt="Not found illustration"
+                        draggable={false}
+                    />
+                </div>
+                <div className="not-found-text">404 Not Found :(</div>
+            </div>
+        </div>
+    );
 }
